74041af25a2b1b9b998895f4eff76f1a68d6c926
kkarthikraj03 <rajkarthik090903@gmail.com>
2023-07-08 00:15:01 +0530

    Fifth Commit

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.DB_URL, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+    console.log("Database successfully connected");
 });
 
 const app = express();
